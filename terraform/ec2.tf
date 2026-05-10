938cb202aab7899b7773e50a42e889df700255ce
amine belkhiri <belkhirimohamed664@gmail.com>
2026-05-10 14:50:14 +0200

    Add CI/CD pipeline and update configs

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,26 +6,35 @@ resource "aws_key_pair" "ecomback_key" {
 
 # EC2 Instance
 resource "aws_instance" "ecomback_ec2" {
-    ami = "ami-0735c191cf914754d"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.ecomback_subnet.id
-    vpc_security_group_ids = [aws_security_group.ecomback_sg.id]
-    key_name =  aws_key_pair.ecomback_key.key_name 
-
-
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update -y
-    apt-get install -y docker.io docker-compose
-    systemctl start docker
-    systemctl enable docker
-  EOF
-
+  ami                         = "ami-0735c191cf914754d"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.ecomback_subnet.id
+  vpc_security_group_ids      = [aws_security_group.ecomback_sg.id]
+  key_name                    = aws_key_pair.ecomback_key.key_name
+  associate_public_ip_address = true
+user_data = <<-EOF
+  #!/bin/bash
+  export HOME=/root
+  apt-get update -y
+  apt-get install -y docker.io docker-compose openjdk-17-jdk git
+  systemctl start docker
+  systemctl enable docker
+  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+  export PATH=$JAVA_HOME/bin:$PATH
+  cd /home/ubuntu
+  git clone https://github.com/chillbro1313/PFA_Nermine-Ala-Amine.git
+  cd PFA_Nermine-Ala-Amine
+  chmod +x mvnw
+  ./mvnw package -DskipTests
+  docker build -t ecomback:1.0 .
+  docker-compose up -d
+EOF
   tags = {
     Name = "ecomback-ec2"
   }
-  }
- # Output the public IP
- output "ec2_public_ip" {
+}
+
+# Output the public IP
+output "ec2_public_ip" {
   value = aws_instance.ecomback_ec2.public_ip
 }
